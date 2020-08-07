6f41a1b6dd3ae693639f538bfffd0a22e9daf655
mubihussain <mubikhan69@gmail.com>
2020-08-07 19:04:00 +0500

    Updates

---
+++ precache-manifest.37c6aedf8dabae336d601cb284005526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0948c20a27f553cc8ce01013d1f8cd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a6e81103fdea3eaea6a",
+    "url": "/monsters-rolodex/static/css/main.9440d9e2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6e81103fdea3eaea6a",
+    "url": "/monsters-rolodex/static/js/main.6cc2ae4c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
